2f4463fc8771f0bd206fcb16145b0fa0ed7668b5
Dhanushtk2002 <dhanuztk03@gmail.com>
2026-01-27 17:17:48 +0530

    Fix DATABASE_URL fallback and SQLite SSL issue

--- student_project/settings.py
+++ student_project/settings.py
@@ -105,11 +105,20 @@ TEMPLATES = [
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ.get(
+            "DATABASE_URL",
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        ),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=bool(os.environ.get("DATABASE_URL")),
     )
 }
+
+
+
+
+
+
 
 # --------------------------------------------------
 # PASSWORD VALIDATION
