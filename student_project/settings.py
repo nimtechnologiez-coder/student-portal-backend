bdf5dcf978067f107eba7c5b6c583010051a5dde
Dhanushtk2002 <dhanuztk03@gmail.com>
2026-02-02 10:12:58 +0530

    Fix ALLOWED_HOSTS for Render domain

--- student_project/settings.py
+++ student_project/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 # --------------------------------------------------
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
+    "localhost,127.0.0.1,.onrender.com"
 ).split(",")
 
 
